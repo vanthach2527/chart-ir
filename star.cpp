3e9ddfe77fef5df2d71db97ba69a75bb96ef52b7
Van Thach <145367679+vanthach2527@users.noreply.github.com>
2025-10-31 16:23:34 +0700

    Auto commit at 2025-10-31T16:23:34+07:00

---
+++ star.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Gay" << endl;
+    return 0;
+    
+}
+ 
---
+++ test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; // Khai báo biến n kiểu số nguyên
+
+    cout << "Nhap mot so nguyen: ";
+    cin >> n; // Nhập giá trị từ bàn phím
+
+    cout << "Ban da nhap: " << n << endl; // In ra giá trị vừa nhập
+
+    return 0;
+}
